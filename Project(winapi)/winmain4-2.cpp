c60940bea14941a6c0bc84fe8be84701f0febd8d
hjl971230 <hjl971230@gmail.com>
2020-08-05 20:31:27 +0900

    update movewatch

--- Project(winapi)/winmain4-2.cpp
+++ Project(winapi)/winmain4-2.cpp
@@ -2,12 +2,23 @@
 #include<math.h>
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void CALLBACK TimeProc(HWND, UINT, UINT, DWORD);
+void CALLBACK MoveProc(HWND, UINT, UINT, DWORD);
+void DrawWatch(HDC);
+void DrawNiddle(HDC);
 HINSTANCE g_hInst;//글로벌 인스턴스핸들값
-LPCTSTR lpszClass = TEXT("Shapes"); //창이름
-int x = 200;
-int y = 200;
+LPCTSTR lpszClass = TEXT("Clock"); //창이름
+#define MID_X 100
+#define MID_Y 100
+#define MIN_X 50
+#define MIN_Y 50
+#define MAX_X 150
+#define MAX_Y 150
+int x = 100;
+int y = 100;
 TCHAR sTime[128] = "\0";
 SYSTEMTIME st;
+bool xflag = true;
+bool yflag = true;
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -53,32 +64,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE://윈도우 생성 시 할당, 초기화 등
 		SetTimer(hWnd, 1, 1000, TimeProc);
 		SendMessage(hWnd, WM_TIMER, 1, 0);
+		SetTimer(hWnd, 2, 50, MoveProc);
+		SendMessage(hWnd, WM_TIMER, 2, 0);
 		return 0;
 	case WM_DESTROY:// 윈도우가 파괴되었다는 메세지
 		PostQuitMessage(0); //GetMessage함수에 WM_QUIT 메시지를 보낸다.
 		KillTimer(hWnd, 1);
+		KillTimer(hWnd, 2);
 		return 0; //WndProc의 Switch는 break 대신 return 0; 를 쓴다.
 	case WM_TIMER:
-		/*GetLocalTime(&st);
+		GetLocalTime(&st);
+		if (st.wHour >= 12)
+			st.wHour -= 12;
 		wsprintf(sTime, TEXT("지금 시간은 %d:%d:%d입니다."), st.wHour, st.wMinute, st.wSecond);
-		InvalidateRect(hWnd, NULL, TRUE);*/
+		InvalidateRect(hWnd, NULL, TRUE);
 		return 0;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		SetTextAlign(hdc, TA_CENTER | TA_BOTTOM);
+		SetTextAlign(hdc, TA_CENTER);
 		TextOut(hdc, 100, 100, sTime, lstrlen(sTime));
-		for (int i = 0; i <= 360; i++)
-		{
-			c_x = cos((i) * 3.14 / 180) * radius;
-			c_y = sin((i) * 3.14 / 180) * radius;
-			SetPixel(hdc, x + 200 + c_x, y + 200 + c_y, RGB(0, 0, 0));
-			if (i % 30 == 0)
-			{
-				wsprintf(sdrawTime, TEXT("%d"), clockdrawnum);
-				TextOut(hdc, x + 200 + c_x, y + 200 + c_y, sdrawTime, lstrlen(sdrawTime));
-				clockdrawnum++;
-			}
-		}
+		DrawWatch(hdc);
+		DrawNiddle(hdc);
 		EndPaint(hWnd, &ps);
 		return 0;
 	}
@@ -88,6 +94,81 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 {
 	GetLocalTime(&st);
+	if (st.wHour >= 12)
+		st.wHour -= 12;
 	wsprintf(sTime, TEXT("지금 시간은 %d:%d:%d입니다."), st.wHour, st.wMinute, st.wSecond);
 	InvalidateRect(hWnd, NULL, TRUE);
+}
+
+void CALLBACK MoveProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
+{
+	if (x <= MIN_X)xflag = true;
+	else if (x >= MAX_X)xflag = false;
+	if (y <= MIN_Y)yflag = false;
+	else if (y >= MAX_Y)yflag = true;
+
+	if (xflag)x++;
+	else x--;
+	if (yflag)y--;
+	else y++;
+
+	InvalidateRect(hWnd, NULL, TRUE);
+}
+
+void DrawWatch(HDC hdc)
+{
+	int radius = 50;
+	int c_x = 0;
+	int c_y = 0;
+	int clockdrawnum = 0;
+	TCHAR sdrawTime[128] = "\0";
+	for (int i = 0; i <= 360; i++)
+	{
+		c_x = cos((i) * 3.14 / 180) * (radius + 24);
+		c_y = sin((i) * 3.14 / 180) * (radius + 24);
+		SetPixel(hdc, x + 200 + c_x, y + 200 + c_y, RGB(0, 0, 0));
+	}
+	for (int i = -90; i <= 270; i++)
+	{
+		c_x = cos((i) * 3.14 / 180) * (radius + 14);
+		c_y = sin((i) * 3.14 / 180) * (radius + 14);
+		MoveToEx(hdc, x + 200, y + 200, NULL);
+		if (i % 30 == 0)
+		{
+			wsprintf(sdrawTime, TEXT("%d"), clockdrawnum);
+			TextOut(hdc, x + 200 + c_x, y + 200 + c_y - 7, sdrawTime, lstrlen(sdrawTime));
+			clockdrawnum++;
+		}
+	}
+}
+
+void DrawNiddle(HDC hdc)
+{
+	int radius = 50;
+	int c_x = 0;
+	int c_y = 0;
+	float hour_go = 0;
+	for (int i = -90; i <= 270; i++)
+	{
+		c_x = cos((i) * 3.14 / 180) * radius;
+		c_y = sin((i) * 3.14 / 180) * radius;
+		MoveToEx(hdc, x + 200, y + 200, NULL);
+		if ((i + 90) % 30 == 0 && (i + 90) / 30 == st.wHour)
+		{
+			hour_go = st.wMinute / 2;
+			if (st.wHour > 6) hour_go *= -1;
+			MoveToEx(hdc, x + 200, y + 200, NULL);
+			LineTo(hdc, x + 200 + c_x + hour_go, y + 200 + c_y + hour_go);
+		}
+		if ((i + 90) % 6 == 0 && (i + 90) / 6 == st.wMinute)
+		{
+			MoveToEx(hdc, x + 200, y + 200, NULL);
+			LineTo(hdc, x + 200 + c_x, y + 200 + c_y);
+		}
+		if ((i + 90) % 6 == 0 && (i + 90) / 6 == st.wSecond)
+		{
+			MoveToEx(hdc, x + 200, y + 200, NULL);
+			LineTo(hdc, x + 200 + c_x, y + 200 + c_y);
+		}
+	}
 }
